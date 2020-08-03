91979672a9291674ffc0602140fccb87ba98a59d
Ben Staker <lychee.technology@benstaker.com>
2020-08-03 16:36:48 +0100

    Section 10 - using refs for DOM access

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,14 +1,11 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 export default (props) => {
-    const images = props.images.map(({ description, id, urls }) => (
-        <img
-            className="ui medium bordered image"
-            src={urls.regular}
-            alt={description}
-            key={id}
-        />
+    const images = props.images.map((image) => (
+        <ImageCard image={image} key={image.id} />
     ));
 
-    return <div className="ui segment">{images}</div>;
+    return <div className="image-list">{images}</div>;
 };
