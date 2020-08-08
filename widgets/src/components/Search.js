ec8dc458d0b86016cebc7c58816473108cb49a28
Ben Staker <lychee.technology@benstaker.com>
2020-08-08 16:12:50 +0100

    Section 12 (part 2) - understanding hooks

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,8 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
     const inputRef = React.createRef();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    format: 'json',
+                    origin: '*',
+                    srsearch: term
+                }
+            });
+
+            if (term) {
+                setResults(data.query.search);
+            }
+        };
+
+        fetchData();
+    }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div className="item" key={result.pageid}>
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    {result.snippet}
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -18,7 +51,7 @@ const Search = () => {
                     />
                 </div>
             </div>
-            <div>List</div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 };
