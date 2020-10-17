a0fa8b69ebe6b3aa252dc99d2fde73944ebc440e
Ben Staker <ben@lychee.technology>
2020-10-17 13:34:54 +0100

    Section 12 (part 4) - understanding hooks

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,11 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ label, options, placeholder, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    // Close dropdown when we click away
+    useEffect(() => {
+        const onBodyClick = (evt) => {
+            if (ref.current.contains(evt.target)) return;
+
+            setOpen(false);
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+    }, []);
 
     const renderedOptions = options.map((option) => {
         return option.value === selected.value ? null : (
-            <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
+            <div
+                key={option.value}
+                className="item"
+                onClick={() => {
+                    onSelectedChange(option);
+                }}
+            >
                 {option.label}
             </div>
         );
@@ -20,7 +42,10 @@ const Dropdown = ({ label, options, placeholder, selected, onSelectedChange }) =
             <div className="field">
                 <label className="label">{label}</label>
                 <div
-                    onClick={() => setOpen(!open)}
+                    ref={ref}
+                    onClick={() => {
+                        setOpen(!open);
+                    }}
                     className={`ui selection dropdown ${open ? 'visible active' : ''}`}
                 >
                     <i className="dropdown icon"></i>
